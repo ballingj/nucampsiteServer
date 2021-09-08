9260eeda6000bdb5a62690dd160d5be12afe36cb
Jeff Ballinger <kumite@Jeffs-MBP.localdomain>
2021-09-07 22:06:20 -0500

    week3 basic auth

--- app.js
+++ app.js
@@ -36,8 +36,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 function auth(req, res, next) {
-  console.log(req.headers);
+  console.log(`req headers value is: `, req.headers);
   const authHeader = req.headers.authorization;
+  console.log(`authHeader value is: `, authHeader);
   if (!authHeader) {
       const err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
@@ -45,16 +46,17 @@ function auth(req, res, next) {
       return next(err);
   }
   const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-        return next(); // authorized
-    } else {
-        const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');      
-        err.status = 401;
-        return next(err);
-    }
+  console.log(`value of auth is ` , auth);
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
 }
  
 app.use(auth);
