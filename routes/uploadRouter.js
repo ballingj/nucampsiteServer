3d86437f0d74ef7f478bd69bbf76a679fb8524e7
Jeff Ballinger <kumite@Jeffs-MBP.localdomain>
2021-09-13 21:57:44 -0500

    File Upload

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -7,19 +7,20 @@ const storage = multer.diskStorage({
         cb(null, 'public/images');
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname)
+        cb(null, file.originalname)  //if not provided, multer will give a random text as filename
     }
 });
 
 const imageFileFilter = (req, file, cb) => {
     if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {   //this is a regex expression looking for certain file extensions -- reject the non-image files
-        return cb(new Error('You can upload only image files!'), false);
+        return cb(new Error('You can upload only image files!'), false);  //false means reject the file
     }
-    cb(null, true);
+    cb(null, true);  //true means multer can accept the file
 };
 
-const upload = multer({ storage: storage, fileFilter: imageFileFilter});
+const upload = multer({ storage: storage, fileFilter: imageFileFilter});  // now call the multer function and use the storage and filter settings created above and configure the multer to enable image file uploads
 
+//now configure the various route methods get, put, post delete
 const uploadRouter = express.Router();
 
 uploadRouter.route('/')
